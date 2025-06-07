e5ed9036094a7b74071cf5f92484fe073ddf0727
Pascal Kleindienst <mail@pascalkleindienst.de>
2025-06-07 15:23:25 +0200

    chore: add missing routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Models\Recipe;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', static function () {
-    return view('welcome');
-})->name('home');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/', static fn () => view('welcome'))->name('home');
 
 Route::middleware(['auth'])->group(function (): void {
     Route::redirect('settings', 'settings/profile');
@@ -25,8 +20,27 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', static function () {
-        return view('dashboard');
-    })->name('dashboard');
+])->group(function (): void {
+    Route::get('/dashboard', static fn () => view('dashboard'))->name('dashboard');
+
+    Route::prefix('recipes')->name('recipes.')->group(function (): void {
+        Volt::route('/', 'recipes.index')->name('index');
+        Volt::route('/create', 'recipes.create')->name('create');
+        // Volt::route('/{recipe}', 'recipes.show')->name('show');
+    });
 });
+
+// Testing route for PDF Template
+if (app()->isLocal()) {
+    Route::get('pdf-test/{recipe}', static function (Recipe $recipe) {
+        if (request()->get('pdf')) {
+            return Pdf::loadView('livewire.recipes.pdf', [
+                'recipe' => $recipe,
+            ])->stream();
+        }
+
+        return view('livewire.recipes.pdf', [
+            'recipe' => $recipe,
+        ]);
+    });
+}
